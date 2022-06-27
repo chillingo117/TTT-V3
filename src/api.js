ae6b522eadd194da685d1dac355f8e96ef9cd0fa
William.Chen <william.chen@phocassoftware.com>
2022-06-27 17:46:58 +1200

    Glorious

--- src/api.js
+++ src/api.js
@@ -31,9 +31,21 @@ class Api{
             .then(response => response.data);
     }
 
+    playGameAi(id)
+    {
+        return this.axiosInstance.post('/game/play/ai', null, { params: {id:id} })
+            .then(response => response.data);
+    }
+
     pingGame(id)
     {
         return this.axiosInstance.get('/game/ping', {params: {id: id}})
+            .then(response => response.data);
+    }
+    
+    jumpTo(id, step)
+    {
+        return this.axiosInstance.post('/game/jump', null, { params: {id:id, step:step} })
             .then(response => response.data);
     }
 }
@@ -41,7 +53,7 @@ class Api{
 export default function CreateApiInstance(){
     let axiosInstance = axios.create({
         baseURL: BASE_URL,
-        timeout: 1000,
+        timeout: 10000,
         headers: {
             'Access-Control-Allow-Origin': '*',
             'Content-Type': 'application/json',
